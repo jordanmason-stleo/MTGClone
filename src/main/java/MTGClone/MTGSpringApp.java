129d4c390469a5241970b3510da0c531ec834238
Gregory Rudolph <rudi@nightmare.haus>
2020-09-28 09:26:52 -0400

    Added Spring Boot web service

---
+++ src/main/java/MTGClone/MTGSpringApp.java
@@ -0,0 +1,20 @@
+package MTGClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MTGSpringApp extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MTGSpringApp.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MTGSpringApp.class, args);
+	}
+
+}
---
+++ src/main/java/MTGClone/controller/HelloController.java
@@ -0,0 +1,23 @@
+package MTGClone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping({"/", "/hello"})
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @PostMapping({"/greet"})
+    public String greet(@RequestParam("name") String name, ModelMap modelMap) {
+        modelMap.put("name", name);
+        return "hello";
+    }
+}
